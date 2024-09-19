6133efd48f8f29800e6fb90436a4135ee89a5169
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-19 21:48:30 +0200

    Initial commit for twintter project.

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-4xl font-bold text-center">Hello World</h1>
+    </div>
+  );
+}
