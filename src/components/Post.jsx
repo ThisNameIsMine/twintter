c3e2d99a73485942c038af1694c0339bb92b5de0
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-26 17:47:06 +0200

    Add like functionality.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,9 @@
 "use client";
+import { deleteDoc, setDoc } from "firebase/firestore";
 import { useEffect } from "react";
 import { CiChat1, CiChat2, CiShare1 } from "react-icons/ci";
 import { FaChartBar, FaHeart, FaTrash } from "react-icons/fa";
-import { GoHeart } from "react-icons/go";
+import { GoHeart, GoHeartFill } from "react-icons/go";
 import {
   HiChartBar,
   HiChat,
@@ -10,8 +11,47 @@ import {
   HiDotsHorizontal,
 } from "react-icons/hi";
 import Moment from "react-moment";
+import { db } from "../../firebase";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  doc,
+} from "firebase/firestore";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
 
 export default function Post({ post }) {
+  const { data: session } = useSession();
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "tweets", post.id, "likes"),
+      (snapshot) => {
+        setLikes(snapshot.docs);
+      }
+    );
+  }, [db]);
+
+  useEffect(() => {
+    setHasLiked(
+      likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+    );
+  }, [likes]);
+
+  async function likePost() {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "tweets", post.id, "likes", session.user.uid));
+    } else {
+      await setDoc(doc(db, "tweets", post.id, "likes", session.user.uid), {
+        username: session.user.username,
+      });
+    }
+  }
+
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
       {/* image */}
@@ -67,10 +107,29 @@ export default function Post({ post }) {
             className="hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
             size={30}
           />
-          <GoHeart
-            className="hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
-            size={30}
-          />
+          <div className="flex items-center">
+            {hasLiked ? (
+              <GoHeartFill
+                onClick={likePost}
+                className="hoverEffect p-2 text-red-600 hover:text-gray-200 hover:bg-white"
+                size={30}
+              />
+            ) : (
+              <GoHeart
+                onClick={likePost}
+                className="hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+                size={30}
+              />
+            )}
+            {likes.length > 0 && (
+              <span
+                className={`text-sm font-bold ${hasLiked && "text-red-600"}`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div>
+
           <CiShare1
             className="hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
             size={30}
